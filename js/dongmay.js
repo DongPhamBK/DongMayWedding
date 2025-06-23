b679a61c9b03fd9de125c9d4eaa168d23341ca44
DongPhamBK <55798267+DongPhamBK@users.noreply.github.com>
2025-06-23 10:47:50 +0700

    update message 3

--- js/dongmay.js
+++ js/dongmay.js
@@ -17,8 +17,8 @@
              const bankElement = document.getElementById('mybank');
             if (greetingElement) {
                 greetingElement.innerText = `Thân mời: ${decodedName}!`;
-                contentElement.innerText = `Sẽ thật hạnh phúc khi được gặp gỡ và đón tiếp ${decodedName} trong ngày trọng đại của chúng tôi(em). Xin gửi đến ${decodedName} lời cảm ơn chân thành và sâu sắc nhất - sự hiện diện của ${decodedName} là niềm vui và là món quà vô cùng ý nghĩa đối với chúng tôi(em)!`;
-                bankElement.innerText = `Cảm ơn ${decodedName} vì đã trở thành một phần quan trọng trong ngày đặc biệt của chúng tôi(em)!`
+                contentElement.innerText = `Sẽ thật hạnh phúc khi được gặp gỡ và đón tiếp ${decodedName} trong ngày trọng đại của chúng tôi (em). Xin gửi đến ${decodedName} lời cảm ơn chân thành và sâu sắc nhất - sự hiện diện của ${decodedName} là niềm vui và là món quà vô cùng ý nghĩa đối với chúng tôi (em)!`;
+                bankElement.innerText = `Cảm ơn ${decodedName} vì đã trở thành một phần quan trọng trong ngày đặc biệt của chúng tôi (em)!`
             }
         }
     });
